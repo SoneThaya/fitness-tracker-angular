f25a1cc223b2abd9dad6295be1519b3b4c282b0d
Sone <sthaya@gmail.com>
2021-03-28 18:25:47 -0700

    controlling active exercise

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,16 +1,27 @@
+import { TrainingService } from './training.service';
+import { Subscription } from 'rxjs/Subscription';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
-  styleUrls: ['./training.component.css']
+  styleUrls: ['./training.component.css'],
 })
 export class TrainingComponent implements OnInit {
   ongoingTraining = false;
+  exerciseSubscription: Subscription;
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) {}
 
   ngOnInit(): void {
+    this.exerciseSubscription = this.trainingService.exerciseChanged.subscribe(
+      (exercise) => {
+        if (exercise) {
+          this.ongoingTraining = true;
+        } else {
+          this.ongoingTraining = false;
+        }
+      }
+    );
   }
-
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,6 +1,10 @@
+import { Subject } from 'rxjs/Subject';
+
 import { Exercise } from './exercise.model';
 
 export class TrainingService {
+  exerciseChanged = new Subject<Exercise>();
+
   private availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 18 },
@@ -18,5 +22,6 @@ export class TrainingService {
     this.runningExercise = this.availableExercises.find(
       (ex) => ex.id === selectedId
     );
+    this.exerciseChanged.next({ ...this.runningExercise });
   }
 }
