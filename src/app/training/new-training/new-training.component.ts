15d9ba0a4ca131244022d93fee0fc05c0e8eb0ee
Sone <sthaya@gmail.com>
2021-03-31 17:33:50 -0700

    setup data fetching from firestore

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,8 +2,9 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { NgForm } from '@angular/forms';
 import { Exercise } from './../exercise.model';
 import { TrainingService } from './../training.service';
-import { Observable } from 'rxjs'
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-training',
@@ -11,8 +12,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
-
+  exercises: Observable<Exercise[]>;
 
   constructor(
     private trainingService: TrainingService,
@@ -21,9 +21,18 @@ export class NewTrainingComponent implements OnInit {
 
   ngOnInit(): void {
     this.exercises = this.db
-      .collection('availableExercises')
-      .valueChanges()
-
+      .collection("availableExercises")
+      .snapshotChanges()
+      .pipe(
+        map((docArray) => {
+          return docArray.map((doc) => {
+            return {
+              id: doc.payload.doc.id,
+              ...(doc.payload.doc.data() as Exercise),
+            };
+          });
+        })
+      );
   }
 
   onStartTraining(form: NgForm) {
