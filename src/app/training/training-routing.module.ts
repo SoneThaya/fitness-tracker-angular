2c67024d5b92fd122cd07364138e2ab877559ddc
Sone <sthaya@gmail.com>
2021-04-04 13:30:48 -0700

    moved auth guard

--- src/app/training/training-routing.module.ts
+++ src/app/training/training-routing.module.ts
@@ -1,10 +1,9 @@
-import { AuthGuard } from './../auth/auth.guard';
 import { TrainingComponent } from './training.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-  { path: '', component: TrainingComponent, canActivate: [AuthGuard] },
+  { path: '', component: TrainingComponent },
 ];
 
 @NgModule({
